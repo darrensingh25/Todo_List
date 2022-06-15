bec09f14efb613f696e424da8d9315610a84d678
ajay <developer@appwrk.co>
2022-06-15 13:19:38 +0530

    todo_list v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo_list';
+
+  list:any[] = [];  // Empty Array of any-type
+
+  addTask(item:string) {   // add the value
+    // console.log(this.list)
+    // console.log(item)
+    this.list.push({id:this.list.length, name:item})
+  }
+
+  removeTask(i:any) {     // Deleting
+    this.list.splice(i,1)
+ }
+
+  // removeTask(id:number) {  // Deleting
+  //   // console.log(id)
+  // this.list = this.list.filter(item => item.id !== id)
+  // }
+
+  // editTask(id:number) {
+  //   //console.log(id)
+  //   this.list = this.list.filter(item => item.id= id)
+
+  // }
+
+//   editTask(id:any) {
+//     const userIndex = this.list.findIndex(item => item.id == id);
+//     if (userIndex != null && userIndex != undefined) {
+//         this.list[userIndex] = this.list;
+//     }
+// }
+  
+editTask(id :any) {
+//  this.list.findIndex((item => item.id ==1))
+this.list.indexOf(id);
+console.log(id)
 }
+  
+}
+  
